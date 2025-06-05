c85889eb0fed869825900116e1b439f0c83305d9
Ma1saa <maisuradzemate7@gmail.com>
2025-06-05 23:46:22 +0400

    Add files via upload

---
+++ day52/homework/homework.py
@@ -0,0 +1,47 @@
+#1 link: https://www.codewars.com/kata/5a00e05cc374cb34d100000d
+
+def reverse_seq(n):
+    if n > 0:
+        return list(range(n, 0, -1))
+    else:
+        return []
+
+#2 link: https://www.codewars.com/kata/5545f109004975ea66000086
+
+def is_divisible(n,x,y):
+    if n % x == 0 and n % y == 0:
+        return True
+    else:
+        return False
+
+#3 link: https://www.codewars.com/kata/55cbd4ba903825f7970000f5
+
+def get_grade(s1, s2, s3):
+    if 90<=(s1 + s2 + s3)/3<=100:
+        return 'A'
+    elif 80<=(s1 + s2 + s3)/3<90:
+        return 'B'
+    elif 70<=(s1 + s2 + s3)/3<80:
+        return 'C'
+    elif 60<=(s1 + s2 + s3)/3<70:
+        return 'D'
+    elif 0<=(s1 + s2 + s3)/3<60:
+        return 'F'
+
+#4 link: https://www.codewars.com/kata/53dbd5315a3c69eed20002dd
+
+def filter_list(l):
+    for i in l[:]:
+        if i == str(i):
+            l.remove(i)
+    return l
+
+#5 link: https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9
+
+def find_short(n):
+    n = n.split()
+    min_len = len(n[0])
+    for i in n:
+        if len(i) < min_len:
+            min_len = len(i)
+
---
+++ day53/classwork/script.js
@@ -0,0 +1,20 @@
+let name = "mate"
+
+let age = 15
+
+let num = 47
+
+let him = "cook"
+
+let num2 = 122
+
+console.log(name + " " + age + " " + num + " " + num2 + " " + him)
+
+console.log(num + 10)
+
+let h1 = document.getElementById("h1")
+let p = document.getElementById("p")
+
+h1.textContent = 'goodby'
+
+p.textContent = 'goodby'
