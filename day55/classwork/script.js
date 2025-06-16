1d9d2490c3efe9a6b05dcda0e93fc3c2800117be
Ma1saa <maisuradzemate7@gmail.com>
2025-06-16 13:51:50 +0400

    comit

---
+++ day55/classwork/script.js
@@ -0,0 +1,15 @@
+document.getElementById('p').textContent = "goodbye"
+
+console.log(document.getElementById('p'))
+
+function five() {
+    console.log(5)
+}
+
+function makebigger(){
+    document.getElementById("img").style.width = '520px'
+}
+
+function makesmaller(){
+    document.getElementById("img").style.width = "220px"
+}
---
+++ day55/homework/script.js
@@ -0,0 +1,15 @@
+function changer(){
+    document.getElementById("butt").style.color = "red"
+    document.getElementById("butt").style.background = "blue"
+    document.getElementById("butt").style.borderRadius = "30%"
+    document.getElementById("butt").style.width = "200px"
+}
+
+function math(){
+    console.log(5 + 3)
+    console.log(34*32)
+    console.log(425148/432)
+}
+
+console.log(math())
+
